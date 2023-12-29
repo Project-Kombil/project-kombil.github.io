d21256f2ca6446279a7c3abc2808375c35bff026
Alefay Kombil <alefay.bunes@gmail.com>
2023-12-29 21:35:40 +1000

    Initial commit for portfolio

---
+++ README.md
@@ -0,0 +1,17 @@
+# Alefay Kombil's Online Portfolio
+
+Welcome to my online portfolio! I'm Alefay Kombil, a front-end developer specializing in UI/UX design. This repository hosts the code for my personal portfolio website, where I showcase my skills, projects, and experiences.
+
+## About Me
+
+I specialize in creating intuitive and engaging user interfaces with a focus on aesthetics and user experience.
+
+## Portfolio
+
+In this portfolio, you will find examples of my work in UI/UX design and front-end development. Feel free to explore and reach out if you have any questions or opportunities!
+
+## Contact Me
+
+You can reach me at alefay.bunes@gmail.com.
+
+Thank you for visiting my portfolio!
---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import data from "../Data.json";
+import About from "../components/About/About";
+import TechnicalSkill from "../components/TechnicalSkill/TechnicalSkill";
+// import Skill from "../components/Skill/Skill";
+import PortfolioSection from "../components/Portfolio/PortfolioSection";
+// import ReviewSection from "../components/Review/ReviewSection";
+import Contact from "../components/Contact/Contact";
+import CreationSection from "../components/Creation/CreationSection";
+import Hero from "../components/Hero/Hero";
+
+const Home = () => {
+	const {
+		heroData,
+		aboutData,
+		serviceData,
+		skillData,
+		creationData,
+		portfolioData,
+		// reviewData,
+		contactData,
+		socialData,
+		socialData2,
+	} = data;
+	return (
+		<>
+			<Hero data={heroData.data} socialData={socialData2} />
+			<About data={aboutData} data-aos="fade-right" />
+			<TechnicalSkill data={serviceData} data-aos="fade-right" />
+			<PortfolioSection data={portfolioData} />
+			<CreationSection data={creationData} data-aos="fade-right" />
+			{/* <Skill data={skillData} data-aos="fade-right" /> */}
+			{/* <ReviewSection data={reviewData} data-aos="fade-right" /> */}
+			<Contact
+				data={contactData}
+				socialData={socialData}
+				data-aos="fade-right"
+			/>
+		</>
+	);
+};
+
+export default Home;
