50b55c63beabb0ea39cd8372ba17538a4c4d0559
Alefay Kombil <alefay.bunes@gmail.com>
2024-07-23 13:38:39 +1000

    Mailing Service Update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,13 @@
-import PropTypes from "prop-types";
-import "./Contact.scss";
 import SectionHeading from "../SectionHeading/SectionHeading";
-import { Icon } from "@iconify/react";
 import SocialLinks from "../SocialLinks/SocialLinks";
+import ContactForm from "./ContactForm";
+import PropTypes from "prop-types";
+import { Icon } from "@iconify/react";
+import "./Contact.scss";
 
 const Contact = ({ data, socialData }) => {
 	const { title, text, subTitle } = data;
+
 	return (
 		<section id="contact">
 			<div className="st-height-b100 st-height-lg-b80"></div>
@@ -20,60 +22,7 @@ const Contact = ({ data, socialData }) => {
 					<div className="col-lg-6">
 						<h3 className="st-contact-title">Get in Touch</h3>
 						<div id="st-alert"></div>
-						<form
-							name="contact"
-							method="POST"
-							data-netlify="true"
-							className="st-contact-form"
-							id="contact-form"
-						>
-							<div className="st-form-field">
-								<input
-									type="text"
-									id="name"
-									name="name"
-									placeholder="Your Name"
-									required
-								/>
-							</div>
-							<div className="st-form-field">
-								<input
-									type="text"
-									id="email"
-									name="email"
-									placeholder="Your Email"
-									required
-								/>
-							</div>
-							<div className="st-form-field">
-								<input
-									type="text"
-									id="subject"
-									name="subject"
-									placeholder="Your Subject"
-									required
-								/>
-							</div>
-							<div className="st-form-field">
-								<textarea
-									cols="30"
-									rows="10"
-									id="msg"
-									name="msg"
-									placeholder="Your Message"
-									required
-								></textarea>
-							</div>
-							<input type="hidden" name="contact" value="contact" />
-							<button
-								className="st-btn st-style1 st-color1"
-								type="submit"
-								id="submit"
-								name="submit"
-							>
-								Send Message
-							</button>
-						</form>
+						<ContactForm/>
 						<div className="st-height-b0 st-height-lg-b30"></div>
 					</div>
 					<div className="col-lg-6">
