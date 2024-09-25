925e33b217d3877092878efdc2540a59473a9d39
Alefay Kombil <alefay.bunes@gmail.com>
2024-09-25 15:07:49 +1000

    Modal Link Update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,42 @@
 import "./Modal.scss";
 
 const Modal = ({ img, title, subTitle, link, modalClose }) => {
-	const modalStyle = {
-		backgroundColor: "rgba(0,0,0,0.8)",
-		display: "block",
-	};
-	return (
-		
-		<div className="modal show fade bd-example-modal-lg" style={modalStyle}>
-			<div className="modal-dialog modal-lg">
-				<div className="modal-content">
-					<div className="modal-header">
-						<h4 className="modal-title">{title}</h4>
-						<button
-							type="button"
-							className="btn-close"
-							onClick={modalClose}
-						></button>
-					</div>
-					<div className="modal-body">
-						<div className="st-flex-center">
-							<img src={img} />
-						</div>
-						<a href={link} className="modal-subtitle"  target="_blank" rel="noopener noreferrer">{subTitle}</a> 
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const modalStyle = {
+    backgroundColor: "rgba(0,0,0,0.8)",
+    display: "block",
+  };
+  return (
+    <div className="modal show fade bd-example-modal-lg" style={modalStyle}>
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4 className="modal-title">{title}</h4>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={modalClose}
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="st-flex-center">
+              <img src={img} alt={title}/>
+            </div>
+            <p className="modal-subtitle">{subTitle}</p>
+             {link && link.trim() !== "" && (
+              <a
+                href={link}
+                className="modal-subtitle"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LINK to Website
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
